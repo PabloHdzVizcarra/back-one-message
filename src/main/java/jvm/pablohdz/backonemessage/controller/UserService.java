2fce4e2ed8066094b27bcd7d95e994f80d0f7fcf
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-24 12:43:49 -0500

    feat create interface to handle logic users

---
+++ src/main/java/jvm/pablohdz/backonemessage/controller/UserService.java
@@ -0,0 +1,7 @@
+package jvm.pablohdz.backonemessage.controller;
+
+import jvm.pablohdz.backonemessage.dto.DataUserRequest;
+
+public interface UserService {
+    void registerUser(DataUserRequest dataUser);
+}
