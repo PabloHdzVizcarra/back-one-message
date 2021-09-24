8d98ad6a53e264d766d36befaba900bb0bdd4fba
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-09-24 09:05:26 -0500

    first commit

---
+++ src/main/java/jvm/pablohdz/backonemessage/BackOneMessageApplication.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.backonemessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOneMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackOneMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jvm/pablohdz/backonemessage/BackOneMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.backonemessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOneMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
